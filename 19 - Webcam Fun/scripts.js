b74d60ea2e31cde742eafb4c5a110014c16395cd
Maksymilian Olszewski <olszewski.maksymilian@gmail.com>
2017-11-17 12:17:25 +0100

    fixed typos

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -11,8 +11,7 @@ function getVideo() {
         .then(localMediaStream => {
         video.src = window.URL.createObjectURL(localMediaStream);
     video.play();
-})
-    .catch(err => {
+}).catch(err => {
         window.alert('Allow camera use!');
     console.log(err);
 });
